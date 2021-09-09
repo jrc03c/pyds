7c3a77ab48733f88747461dd975b6aef3941d8d0
jrc03c <jrc03c@pm.me>
2021-09-09 09:14:06 -0500

    ...

--- tests/test_chop.py
+++ tests/test_chop.py
@@ -2,20 +2,27 @@ import unittest
 from python_data_science_helpers import chop
 from numpy.random import *
 
+
 class ChopTestCase(unittest.TestCase):
-  def test(self):
-    a = random() + 1
-    self.assertEqual(chop(a), a, msg="Cannot chop a positive number!")
+    def test(self):
+        a = random() + 1
+        self.assertEqual(chop(a), a, msg="Cannot chop a positive number!")
 
-    b = -1 - random()
-    self.assertEqual(chop(b), b, msg="Cannot chop a negative number!")
+        b = -1 - random()
+        self.assertEqual(chop(b), b, msg="Cannot chop a negative number!")
 
-    self.assertEqual(chop(0), 0, msg="Cannot chop zero!")
-    self.assertEqual(chop(1e-20), 0, msg="Cannot chop a very small positive number!")
-    self.assertEqual(chop(-(1e-20)), 0, msg="Cannot chop a very small negative number!")
+        self.assertEqual(chop(0), 0, msg="Cannot chop zero!")
 
-  def testErrors(self):
-    self.assertRaises(AssertionError, chop, "suey")
-    self.assertRaises(AssertionError, chop, True)
-    self.assertRaises(AssertionError, chop, lambda x: x)
-    self.assertRaises(AssertionError, chop, {"foo": "bar"})
+        self.assertEqual(
+            chop(1e-20), 0, msg="Cannot chop a very small positive number!"
+        )
+
+        self.assertEqual(
+            chop(-(1e-20)), 0, msg="Cannot chop a very small negative number!"
+        )
+
+    def testErrors(self):
+        self.assertRaises(AssertionError, chop, "suey")
+        self.assertRaises(AssertionError, chop, True)
+        self.assertRaises(AssertionError, chop, lambda x: x)
+        self.assertRaises(AssertionError, chop, {"foo": "bar"})
--- tests/test_correl.py
+++ tests/test_correl.py
@@ -3,12 +3,23 @@ from python_data_science_helpers import correl
 from numpy import *
 from numpy.random import *
 
-class CorrelTestCase(unittest.TestCase):
-  def test(self):
-    a = random(size=999999)
-    b = random(size=999999)
-    self.assertLess(abs(correl(a, b)), 0.01, msg="The correlation of two random vectors is NOT close to 0!")
 
-    a = random(size=999999)
-    b = a + 1e-5 * normal(size=999999)
-    self.assertGreater(abs(correl(a, b)), 0.99, msg="The correlation of two almost-identical vectors is NOT close to 1!")
+class CorrelTestCase(unittest.TestCase):
+    def test(self):
+        a = random(size=999999)
+        b = random(size=999999)
+
+        self.assertLess(
+            abs(correl(a, b)),
+            0.01,
+            msg="The correlation of two random vectors is NOT close to 0!",
+        )
+
+        a = random(size=999999)
+        b = a + 1e-5 * normal(size=999999)
+
+        self.assertGreater(
+            abs(correl(a, b)),
+            0.99,
+            msg="The correlation of two almost-identical vectors is NOT close to 1!",
+        )
