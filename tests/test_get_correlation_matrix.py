a296effee32aea63ab48fab265e442b7376ce0c0
jrc03c <jrc03c@pm.me>
2021-09-10 08:23:48 -0500

    ...

--- tests/test_get_correlation_matrix.py
+++ tests/test_get_correlation_matrix.py
@@ -28,5 +28,49 @@ class GetCorrelationMatrixTestCase(unittest.TestCase):
             msg="The computed correlation matrix should not have any values greater than 1!",
         )
 
+        d = normal(size=1000)
+        x = [d]
+
+        for i in range(0, 10):
+            x.append(d + 0.0001 * normal(size=1000))
+
+        x = transpose(x)
+        c = getCorrelationMatrix(x)
+
+        self.assertGreater(
+            min(c),
+            0.95,
+            msg="The correlation matrix computed from a matrix with highly-correlated columns should not contain values less than 0.95!",
+        )
+
+        self.assertEqual(
+            max(c),
+            1,
+            msg="The correlation matrix computed from a matrix with highly-correlated columns should have a main diagonal of ones!",
+        )
+
     def testErrors(self):
-        pass
+        self.assertRaises(AssertionError, getCorrelationMatrix, [1, 2, 3], [4, 5, 6])
+
+        self.assertRaises(
+            AssertionError,
+            getCorrelationMatrix,
+            normal(size=[3, 3, 3]),
+            normal(size=[3, 3, 3]),
+        )
+
+        self.assertRaises(AssertionError, getCorrelationMatrix, "foo", "bar")
+
+        self.assertRaises(AssertionError, getCorrelationMatrix, True, False)
+
+        self.assertRaises(
+            AssertionError, getCorrelationMatrix, 123, 456,
+        )
+
+        self.assertRaises(
+            AssertionError, getCorrelationMatrix, {"foo": "bar"}, {"hello": "world"}
+        )
+
+        self.assertRaises(
+            AssertionError, getCorrelationMatrix, lambda x: x * 2, lambda x: x * 3,
+        )
