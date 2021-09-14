a1a0b1cdf0886b0202a5db03cc20c6f1ee6fce33
jrc03c <jrc03c@pm.me>
2021-09-14 08:39:17 -0500

    ...

---
+++ tests/test_is_a_number.py
@@ -0,0 +1,23 @@
+import unittest
+from pyds import isANumber
+
+
+class IsANumberTestCase(unittest.TestCase):
+    def test(self):
+        numbers = [1, 0, -1, 234.567, -234.567, 1e20, 1e-20]
+
+        for number in numbers:
+            self.assertTrue(isANumber(number), msg="Fails to identify numbers!")
+
+        others = [
+            "foo",
+            True,
+            False,
+            None,
+            {"hello": "world"},
+            lambda x: 2 * x,
+            [[[234]]],
+        ]
+
+        for item in others:
+            self.assertFalse(isANumber(item), msg="Fails to identify non-numbers!")
