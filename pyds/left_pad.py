a3787341c33db4cb0585376be64659a551d84f3a
jrc03c <jrc03c@pm.me>
2021-09-14 10:08:23 -0500

    ...

--- pyds/left_pad.py
+++ pyds/left_pad.py
@@ -1,8 +1,14 @@
 from .is_a_number import *
 
-def leftPad(n, max):
-  assert type(n) is int, "`n` must be an integer!"
-  assert type(max) is int, "`max` must be an integer!"
 
-  numberOfZeros = len(str(max)) - len(str(n))
-  return "0" * numberOfZeros + str(n)
+def leftPad(n, max):
+    assert type(n) is int, "`n` must be a whole number!"
+    assert n >= 0, "`n` must be a whole number!"
+
+    assert type(max) is int, "`max` must be a whole number!"
+    assert max >= 0, "`max` must be a whole number!"
+
+    assert n <= max, "`n` must be less than or equal to `max`!"
+
+    numberOfZeros = len(str(max)) - len(str(n))
+    return "0" * numberOfZeros + str(n)
