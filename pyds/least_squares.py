36ec10034f06600c8f77c9816d76f80b40c8eb33
jrc03c <jrc03c@pm.me>
2021-09-14 10:01:25 -0500

    ...

--- pyds/least_squares.py
+++ pyds/least_squares.py
@@ -1,7 +1,9 @@
-from .is_a_tensor import *
+from .is_a_matrix import *
+from .is_a_vector import *
 from numpy.linalg import lstsq
 
+
 def leastSquares(a, b):
-  assert isATensor(a), "`a` must be a vector, matrix, or tensor!"
-  assert isATensor(b), "`b` must be a vector, matrix, or tensor!"
-  return lstsq(a, b, rcond=None)[0]
+    assert isAMatrix(a), "`a` must be a matrix!"
+    assert isAVector(b) or isAMatrix(b), "`b` must be a vector or matrix!"
+    return lstsq(a, b, rcond=None)[0]
