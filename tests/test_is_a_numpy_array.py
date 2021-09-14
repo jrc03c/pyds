4f05077387b842e1efa75213342c2d1be4fd16db
jrc03c <jrc03c@pm.me>
2021-09-14 08:43:09 -0500

    ...

---
+++ tests/test_is_a_numpy_array.py
@@ -0,0 +1,32 @@
+import unittest
+from pyds import isANumpyArray
+from numpy import *
+from numpy.random import *
+from pandas import DataFrame as DF
+
+
+class IsANumpyArrayTestCase(unittest.TestCase):
+    def test(self):
+        arrays = [
+            normal(size=100),
+            random(size=[2, 3, 4, 5]),
+            array([2, 3, 4, 5]),
+            ndarray([2, 3, 4, 5]),
+        ]
+
+        for arr in arrays:
+            self.assertTrue(isANumpyArray(arr))
+
+        others = [
+            234,
+            True,
+            False,
+            "foo",
+            lambda x: x * 2,
+            {"hello": "world"},
+            [2, 3, 4, 5],
+            DF(normal(size=[100, 100])),
+        ]
+
+        for item in others:
+            self.assertFalse(isANumpyArray(item))
