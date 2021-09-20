5d0fe1c0ec627e832417f8dc96b5bd49f18a6d82
jrc03c <jrc03c@pm.me>
2021-09-20 08:40:19 -0500

    ...

--- pyds/sign.py
+++ pyds/sign.py
@@ -1,8 +1,25 @@
 from .is_a_number import *
+from .is_a_tensor import *
+from .is_a_numpy_array import *
+from .is_a_pandas_series import *
+from .is_a_pandas_dataframe import *
+
 
 def sign(x):
-  assert isANumber(x), "`x` must be a number!"
+    if isATensor(x):
+        if isAPandasSeries(x) or isAPandasDataFrame(x):
+            x = x.values.tolist()
 
-  if x > 0: return 1
-  if x < 0: return -1
-  return 0
+        if isANumpyArray(x):
+            x = x.tolist()
+
+        return [sign(v) for v in x]
+
+    else:
+        assert isANumber(x), "`x` must be a number!"
+
+        if x > 0:
+            return 1
+        if x < 0:
+            return -1
+        return 0
---
+++ tests/test_sign.py
@@ -0,0 +1,42 @@
+import unittest
+from pyds import sign, sort, set, isEqual
+from numpy import vectorize
+from numpy.random import random
+
+round = vectorize(round)
+
+
+class SignTestCase(unittest.TestCase):
+    def test(self):
+        self.assertEqual(sign(234), 1, msg="Failed to get the sign of a number!")
+        self.assertEqual(sign(-234), -1, msg="Failed to get the sign of a number!")
+        self.assertEqual(sign(0), 0, msg="Failed to get the sign of a number!")
+
+        a = [2, -3, 4, -5, 6, 0]
+        b = [1, -1, 1, -1, 1, 0]
+
+        self.assertTrue(
+            isEqual(sign(a), b), msg="Failed to get the signs of numbers in an array!"
+        )
+
+        a = round(random(size=[10, 10, 10]) * 2 - 1).tolist()
+        b = sign(a)
+
+        self.assertTrue(
+            isEqual(a, b), msg="Failed to get the signs of numbers in an array!"
+        )
+
+    def testErrors(self):
+        wrongs = [
+            "foo",
+            True,
+            False,
+            None,
+            {"hello": "world"},
+            lambda x: x * 2,
+            [2, 3, "four"],
+            [[[2, 3, None]]],
+        ]
+
+        for item in wrongs:
+            self.assertRaises(AssertionError, sign, item)
