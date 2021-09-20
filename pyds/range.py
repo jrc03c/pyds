e111b186d99ea3330eaa613b6327a698c35e50d9
jrc03c <jrc03c@pm.me>
2021-09-20 07:10:10 -0500

    ...

--- pyds/range.py
+++ pyds/range.py
@@ -1,4 +1,16 @@
-oldRange = range
+from .is_a_number import *
+from .sign import *
+from numpy import arange
+
 
 def range(a, b, step=1):
-  return list(oldRange(a, b, step))
+    assert isANumber(a), "`a` must be a number!"
+    assert isANumber(b), "`b` must be a number!"
+    assert isANumber(step), "`step` must be a number!"
+    assert step != 0, "`step` cannot be zero!"
+
+    assert sign(step) == sign(
+        b - a
+    ), "The sign of `step` must be consistent with the direction from `a` to `b`!"
+
+    return arange(a, b, step).tolist()
