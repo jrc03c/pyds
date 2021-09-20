8b236711e6842dcf88337cdd83c6b08b96320761
jrc03c <jrc03c@pm.me>
2021-09-20 09:59:27 -0500

    ...

--- pyds/reverse.py
+++ pyds/reverse.py
@@ -1,6 +1,7 @@
 from .is_a_number import *
 from .is_a_tensor import *
 from .is_a_string import *
+from numpy import array
 
 
 def reverse(x):
@@ -21,7 +22,7 @@ def reverse(x):
         for i in range(0, len(x)):
             out.append(x[-i - 1])
 
-        return out
+        return array(out)
 
     raise AssertionError(
         "You must pass a number, string, or array to the `reverse` function!"
