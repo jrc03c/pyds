ef5e35072454024e56c329843aba6270b69ea475
jrc03c <jrc03c@pm.me>
2021-09-20 08:01:18 -0500

    ...

--- pyds/shuffle.py
+++ pyds/shuffle.py
@@ -1,6 +1,15 @@
+from .is_a_tensor import *
+from .is_a_pandas_series import *
+from .is_a_pandas_dataframe import *
 import numpy
 
-def shuffle(arr):
-  out = numpy.copy(arr)
-  numpy.random.shuffle(out)
-  return list(out)
+
+def shuffle(x):
+    assert isATensor(x), "`x` must be a tensor!"
+
+    if isAPandasSeries(x) or isAPandasDataFrame(x):
+        x = x.values
+
+    out = numpy.copy(x)
+    numpy.random.shuffle(out)
+    return out.tolist()
---
+++ tests/test_shuffle.py
@@ -0,0 +1,49 @@
+import unittest
+from pyds import shuffle, isEqual, sort, set
+from numpy import shape
+from numpy.random import normal
+from pandas import Series, DataFrame
+
+
+class ShuffleTestCase(unittest.TestCase):
+    def test(self):
+        a = normal(size=1000)
+        b = shuffle(a)
+        self.assertFalse(isEqual(a, b), msg="Failed to shuffle an array!")
+        self.assertTrue(isEqual(shape(a), shape(b)), msg="Failed to shuffle an array!")
+
+        self.assertTrue(
+            isEqual(sort(set(a)), sort(set(b))), msg="Failed to shuffle an array!"
+        )
+
+        a = normal(size=[10, 10, 10, 10])
+        b = shuffle(a)
+
+        self.assertFalse(isEqual(a, b), msg="Failed to shuffle an array!")
+        self.assertTrue(isEqual(shape(a), shape(b)), msg="Failed to shuffle an array!")
+
+        self.assertTrue(
+            isEqual(sort(set(a)), sort(set(b))), msg="Failed to shuffle an array!"
+        )
+
+        a = Series(normal(size=1000))
+        b = shuffle(a)
+        self.assertTrue(isEqual(sort(set(a)), sort(set(b))))
+
+        a = DataFrame(normal(size=[10, 10]))
+        b = shuffle(a)
+        self.assertTrue(isEqual(sort(set(a)), sort(set(b))))
+
+    def testErrors(self):
+        wrongs = [
+            234,
+            "foo",
+            True,
+            False,
+            None,
+            {"hello": "world"},
+            lambda x: x * 2,
+        ]
+
+        for item in wrongs:
+            self.assertRaises(AssertionError, shuffle, item)
