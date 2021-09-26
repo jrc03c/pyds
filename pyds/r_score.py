0eb5bdfa67a9f33cbc9d32f4f74c51fc3f603ef9
jrc03c <jrc03c@pm.me>
2021-09-26 08:11:26 -0500

    ...

--- pyds/r_score.py
+++ pyds/r_score.py
@@ -1,4 +1,5 @@
 from .is_a_tensor import *
+from .is_a_pandas_series import *
 from .is_a_pandas_dataframe import *
 from .is_a_numpy_array import *
 from .is_binary import *
@@ -12,17 +13,25 @@ def mode(x):
     return scipy.stats.mode(x)[0]
 
 
-def rScore(true, pred):
+def rScore(true, pred, baseline=None):
+    if baseline is None:
+        baseline = true
+
     assert isATensor(true), "`true` must be a vector, matrix, or tensor!"
     assert isATensor(pred), "`pred` must be a vector, matrix, or tensor!"
+    assert isATensor(baseline), "`baseline` must be a vector, matrix, or tensor!"
     assert containsOnlyNumbers(true), "`true` must contain only numbers!"
     assert containsOnlyNumbers(pred), "`pred` must contain only numbers!"
+    assert containsOnlyNumbers(baseline), "`baseline` must contain only numbers!"
 
-    if isAPandasDataFrame(true):
+    if isAPandasSeries(true) or isAPandasDataFrame(true):
         true = true.values
 
-    if isAPandasDataFrame(pred):
+    if isAPandasSeries(pred) or isAPandasDataFrame(pred):
         pred = pred.values
+
+    if isAPandasSeries(baseline) or isAPandasDataFrame(baseline):
+        baseline = baseline.values
 
     if not isANumpyArray(true):
         true = array(true)
@@ -30,14 +39,17 @@ def rScore(true, pred):
     if not isANumpyArray(pred):
         pred = array(pred)
 
-    if isBinary(true) and isBinary(pred):
+    if not isANumpyArray(baseline):
+        baseline = array(baseline)
+
+    if isBinary(baseline):
         helper = mode
 
     else:
         helper = mean
 
     num = sum((true - pred) ** 2)
-    den = sum((true - helper(true)) ** 2)
+    den = sum((true - helper(baseline)) ** 2)
 
     if den == 0:
         return 0
