c2918fffbfae2f143b2cd7e2d4d0b7fe3b305ac9
jrc03c <jrc03c@pm.me>
2021-11-10 11:27:56 -0600

    ...

--- tests/test_is_jagged.py
+++ tests/test_is_jagged.py
@@ -1,14 +1,32 @@
 import unittest
 from pyds import isJagged
+from numpy.random import normal
 
 
 class IsJaggedTestCase(unittest.TestCase):
     def test(self):
-        a = [[[2, 3, 4]], [[5, 6, 7]], [[8, 9, 0]]]
-        self.assertFalse(isJagged(a))
+        jag = normal(size=[2, 3, 4, 5]).tolist()
+        jag[1][2][3] = jag[1][2][3][:2]
 
-        b = [[2], [3, 4], [5, 6, 7]]
-        self.assertTrue(isJagged(b))
+        pairs = [
+            [jag, True],
+            [[[[2, 3, 4]], [[5, 6, 7]], [[8, 9, 0]]], False],
+            [[[2], [3, 4], [5, 6, 7]], True],
+            [True, False],
+            [False, False],
+            [None, False],
+            ["foo", False],
+            [234, False],
+            [{}, False],
+            [[], False],
+            [lambda x: x, False],
+        ]
+
+        for pair in pairs:
+            if pair[1] is True:
+                self.assertTrue(isJagged(pair[0]))
+            else:
+                self.assertFalse(isJagged(pair[1]))
 
     def testErrors(self):
         pass
