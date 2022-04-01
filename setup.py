e0bdad8504976ad0ccecf3db235311c91c5847f0
jrc03c <jrc03c@pm.me>
2022-04-01 16:56:45 -0500

    ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyds",
-    version="0.0.20",
+    version="0.0.21",
     description="A collection of helper tools for doing data science stuff. Please don't judge my ignorance too harshly.",
     url="https://github.com/jrc03c/pyds",
     author="jrc03c",
@@ -10,4 +10,5 @@ setup(
     license="none",
     packages=["pyds"],
     classifiers=["Topic :: Scientific/Engineering :: Mathematics",],
+    include_package_data=True,
 )
