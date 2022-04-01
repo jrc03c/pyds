03586cc1f9aa50974c291a6ba4555b20e81572c4
jrc03c <jrc03c@pm.me>
2022-04-01 16:52:50 -0500

    I'm temporarily removing the "install_requires" values in setup.py. If it all fails horribly, then roll back to the commit before this one.

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 setup(
     name="pyds",
-    version="0.0.19",
-    description="A collection of helper tools for doing data science stuff. Please don't judge me too harshly for my ignorance.",
-    url="https://github.com/jrc03c/python-data-science-tools",
+    version="0.0.20",
+    description="A collection of helper tools for doing data science stuff. Please don't judge my ignorance too harshly.",
+    url="https://github.com/jrc03c/pyds",
     author="jrc03c",
     author_email="jrc03c@pm.me",
     license="none",
     packages=["pyds"],
-    install_requires=["numpy", "scipy", "pandas", "matplotlib"],
     classifiers=["Topic :: Scientific/Engineering :: Mathematics",],
 )
