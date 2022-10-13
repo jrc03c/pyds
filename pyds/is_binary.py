d99edb9aa843e85b1e61ef44701da4213bb3e11b
jrc03c <jrc03c@pm.me>
2022-10-13 09:53:44 -0500

    ...

--- pyds/is_binary.py
+++ pyds/is_binary.py
@@ -1,21 +1,17 @@
 from .flatten import flatten
 from .is_a_number import isANumber
 from .is_a_tensor import isATensor
-from .set import set
-from .sort import sort
 
 
 def isBinary(x):
     if isATensor(x):
-        temp = sort(set(flatten(x)))
+        temp = flatten(x)
 
-        if len(temp) == 1:
-            return temp[0] == 0 or temp[0] == 1
+        for v in temp:
+            if v != 0 and v != 1:
+                return False
 
-        if len(temp) == 2:
-            return temp[0] == 0 and temp[1] == 1
-
-        return False
+        return True
 
     elif isANumber(x):
         return x == 0 or x == 1
