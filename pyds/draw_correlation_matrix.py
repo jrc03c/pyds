baece74e1e3f326e5fde0f937c12791e6481e854
jrc03c <jrc03c@pm.me>
2023-05-29 20:18:54 -0500

    ...

--- pyds/draw_correlation_matrix.py
+++ pyds/draw_correlation_matrix.py
@@ -1,10 +1,18 @@
-from .get_correlation_matrix import getCorrelationMatrix
+from .is_a_numpy_array import isANumpyArray
+from .is_a_pandas_dataframe import isAPandasDataFrame
+from numpy import array
+import seaborn
 import matplotlib.pyplot as plot
 
 
-def drawCorrelationMatrix(a, b):
-    temp = getCorrelationMatrix(a, b)
-    plot.pcolormesh(temp, vmin=-1, vmax=1)
+def drawCorrelationMatrix(c):
+    if type(c) == list:
+        c = array(c)
+
+    assert isANumpyArray(c) or isAPandasDataFrame(c), "`c` must be a 2-dimensional list, numpy array, or pandas DataFrame!"
+
+    assert len(c.shape) == 2, "`c` must be a 2-dimensional list, numpy array, or pandas DataFrame!"
+
+    seaborn.heatmap(c, annot=True, cmap=seaborn.diverging_palette(0, 120, as_cmap=True))
     plot.show()
     plot.clf()
-    return temp
