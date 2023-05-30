d478538a8752e0e8bef697e7e4ebd3752ebbfbd4
jrc03c <jrc03c@pm.me>
2023-05-29 20:28:59 -0500

    ...

--- pyds/draw_correlation_matrix.py
+++ pyds/draw_correlation_matrix.py
@@ -1,3 +1,4 @@
+from .flatten import flatten
 from .is_a_numpy_array import isANumpyArray
 from .is_a_pandas_dataframe import isAPandasDataFrame
 from numpy import array, max, min
@@ -12,6 +13,8 @@ def drawCorrelationMatrix(c, filename=None):
 
     assert len(c.shape) == 2, "`c` must be a 2-dimensional list, numpy array, or pandas DataFrame!"
 
-    assert min(c) >= -1 and max(c) <= 1, "`c` must be a correlation matrix, meaning that it must have values between -1 and 1!"
+    cFlat = flatten(c)
+
+    assert min(cFlat) >= -1 and max(cFlat) <= 1, "`c` must be a correlation matrix, meaning that it must have values between -1 and 1!"
 
     seaborn.heatmap(c, annot=True, cmap=seaborn.diverging_palette(0, 120, as_cmap=True))
