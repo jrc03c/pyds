c0debb6de2365f8fb3883369d74ae6e1da7c65f0
jrc03c <jrc03c@pm.me>
2024-04-11 17:59:02 -0500

    ...

--- pyds/correl.py
+++ pyds/correl.py
@@ -1,12 +1,12 @@
 from scipy.stats import pearsonr
 
-from .contains_only_numbers import containsOnlyNumbers
-from .is_a_vector import isAVector
+from .contains_only_numbers import contains_only_numbers
+from .is_a_vector import is_a_vector
 
 
 def correl(a, b):
-    assert isAVector(a), "`a` must be a vector!"
-    assert isAVector(b), "`b` must be a vector!"
-    assert containsOnlyNumbers(a), "`a` must contain only numbers!"
-    assert containsOnlyNumbers(b), "`b` must contain only numbers!"
+    assert is_a_vector(a), "`a` must be a vector!"
+    assert is_a_vector(b), "`b` must be a vector!"
+    assert contains_only_numbers(a), "`a` must contain only numbers!"
+    assert contains_only_numbers(b), "`b` must contain only numbers!"
     return pearsonr(a, b)[0]
